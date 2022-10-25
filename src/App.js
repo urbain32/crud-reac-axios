4b87f7091624612391cffaebd02dde082871d427
Mac <mac@Mac.local>
2022-10-25 16:09:05 +0200

    react axios get data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
 function App() {
+  const [posts, setPosts] = useState([]);
+  const apiEndPoint = 'https://jsonplaceholder.typicode.com/posts';
+  useEffect(() => {
+    const getPost = async () => {
+      const { data: res } = await axios.get(apiEndPoint);
+      setPosts(res);
+    };
+    getPost();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h2>There are {posts.length} poost in database. </h2>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Update</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post) => (
+            <tr key={post.id}>
+              <td>{post.title}</td>
+              <td>
+                <button className='btn btn-outline-info btn-sm'>Update</button>
+              </td>
+              <td>
+                <button className='btn btn-outline-danger btn-sm'>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
